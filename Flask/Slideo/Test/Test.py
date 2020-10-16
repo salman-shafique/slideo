3860ced8393c12b8402cc12bf3f5ce7f573d140f
Alperen Berat DURMUŞ <44159011+alperendurmus@users.noreply.github.com>
2020-10-16 13:17:36 +0300

    database connection for python

--- Flask/Slideo/Test/Test.py
+++ Flask/Slideo/Test/Test.py
@@ -1,7 +1,19 @@
 import os
+import mysql.connector
 
+from .. import env
+
+
+
+# DOWNLOAD THE PRESENTATION
 class Test(object):
+    def __init__(self):
+        self.db = mysql.connector.connect(host=env.DB_HOST,port=env.DB_PORT, user=env.DB_USER, passwd=env.DB_PASSWORD, database=env.DB_NAME)
 
     def test(self, args):
-        return [args,os.getcwd()]
+        cur =  self.db.cursor()
+        cur.execute("SELECT * FROM doctrine_migration_versions WHERE 1")
+        data = cur.fetchall()
+        return data
+    
 
--- Flask/Slideo/env.py
+++ Flask/Slideo/env.py
@@ -1,5 +1,28 @@
+import os
+
 PUBLIC_PATH = "/var/www/app/public"
 
-# https://thenounproject.com/
-THENOUNPROJECT_KEY = "9afa37c3d8c44729982592d62e9206ad"
-THENOUNPROJECT_SECRET = "c9ad106df7e7413e9b5a821d2dbcc19c"
+
+
+# parse symfony's .env file
+if os.path.exists("/var/www/app/.env.local"):
+    # First check local env
+    env_file = open("/var/www/app/.env.local", "r")
+else:
+    env_file = open("/var/www/app/.env", "r")
+
+for line in env_file.readlines():
+    line = line.strip().replace("\n","")
+    if "DATABASE_URL" in line:
+        DB_HOST = line.split("@")[1].split(":")[0]
+        DB_PORT = int(line.split(":")[-1].split("/")[0])
+        DB_USER = line.split("//")[1].split(":")[0]
+        DB_PASSWORD = line.split("@")[0].split(":")[-1]
+        DB_NAME = line.split("/")[-1].split("?")[0]
+        continue
+    if "THENOUNPROJECT_KEY" in line:
+        THENOUNPROJECT_KEY = line.split("=")[1].strip()
+        continue
+    if "THENOUNPROJECT_SECRET" in line:
+        THENOUNPROJECT_SECRET = line.split("=")[1].strip()
+        continue
