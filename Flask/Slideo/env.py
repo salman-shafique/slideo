676a1bc44959a3ebd043e18c4c939548d71a0c23
Alperen Berat DURMUŞ <44159011+alperendurmus@users.noreply.github.com>
2020-10-18 20:52:41 +0300

    Update env.py

--- Flask/Slideo/env.py
+++ Flask/Slideo/env.py
@@ -3,8 +3,7 @@ import os
 PUBLIC_PATH = "/var/www/app/public"
 
 
-
-# parse symfony's .env file 
+# parse symfony's .env file
 if os.path.exists("/var/www/app/.env.local"):
     # First check local env
     env_file = open("/var/www/app/.env.local", "r")
@@ -12,17 +11,17 @@ else:
     env_file = open("/var/www/app/.env", "r")
 
 for line in env_file.readlines():
-    line = line.strip().replace("\n","")
-    if "DATABASE_URL" in line:
+    line = line.strip().replace("\n", "")
+    if "DATABASE_URL" == line[: len("DATABASE_URL")]:
         DB_HOST = line.split("@")[1].split(":")[0]
         DB_PORT = int(line.split(":")[-1].split("/")[0])
         DB_USER = line.split("//")[1].split(":")[0]
         DB_PASSWORD = line.split("@")[0].split(":")[-1]
         DB_NAME = line.split("/")[-1].split("?")[0]
         continue
-    if "THENOUNPROJECT_KEY" in line:
+    if "THENOUNPROJECT_KEY" == line[: len("THENOUNPROJECT_KEY")]:
         THENOUNPROJECT_KEY = line.split("=")[1].strip()
         continue
-    if "THENOUNPROJECT_SECRET" in line:
+    if "THENOUNPROJECT_SECRET" == line[: len("THENOUNPROJECT_SECRET")]:
         THENOUNPROJECT_SECRET = line.split("=")[1].strip()
         continue
