c45dc4440f2a033ca6fc420eb4fa6e55d02f048b
Alperen Berat DURMUŞ <44159011+alperendurmus@users.noreply.github.com>
2020-12-26 22:15:43 +0300

    Stable direction

--- assets/editor/js/entry/editor_buttons/remove_format.js
+++ assets/editor/js/entry/editor_buttons/remove_format.js
@@ -3,12 +3,11 @@ import add_event from "Editor/js/utils/add_event";
 
 add_event("#entry_remove_format", "click", function () {
     let area = document.getElementById("area");
-    let firstContent = document.getElementById("first-area");
-    [area, firstContent].forEach(input => {
-        if (input)
-            input.querySelectorAll("*").forEach(e => {
-                e.setAttribute("style", "");
-                e.setAttribute("class", "");
-            });
-    });
+
+    if (area)
+        area.querySelectorAll("*").forEach(e => {
+            e.setAttribute("style", "");
+            e.setAttribute("class", "");
+        });
+
 })
--- assets/editor/js/entry/input_field/switch_direction.js
+++ assets/editor/js/entry/input_field/switch_direction.js
@@ -1,24 +1,19 @@
 import session from "Editor/js/session";
+import "Editor/css/direction.css";
+
 
 export default function switch_direction(dir) {
-    let inline_editors = document.querySelectorAll(".inline-editor");
-    let area = document.getElementById("area");
-    let slide_title = document.getElementById("slideTitle");
-    let first_area = document.getElementById("first-area");
-    let first_slide_title = document.getElementById("first-slideTitle");
 
-    if (area)
-        area.style.direction = dir;
-    if (first_area)
-        first_area.style.direction = dir;
-    if (first_slide_title)
-        first_slide_title.style.direction = dir;
-    if (inline_editors)
-        inline_editors.forEach(inline_editor => {
-            inline_editor.style.direction = dir;
-        });
-    if (slide_title)
-        slide_title.style.direction = dir;
+    let previousDirection;
+    if (dir == "rtl")
+        previousDirection = "ltr";
+    else
+        previousDirection = "rtl";
+
+    document.querySelectorAll("." + previousDirection).forEach(e => {
+        e.classList.remove(previousDirection);
+        e.classList.add(dir);
+    });
 
     session.DIRECTION = dir;
 }
