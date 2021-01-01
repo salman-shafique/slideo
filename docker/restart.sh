07b79ac4e9482fc2e2a33190912cf05898fde380
Alperen Berat DURMUŞ <44159011+alperendurmus@users.noreply.github.com>
2021-01-01 23:26:07 +0300

    Update restart.sh

--- docker/restart.sh
+++ docker/restart.sh
@@ -4,7 +4,7 @@ cd /var/www/app
 symfony server:stop;
 php bin/console cache:clear;
 symfony server:start -d;
-chmod -R 777 var/*;
+chmod -R 777 var;
 chmod -R 777 public/styles;
 chmod -R 777 public/icons;
 chmod -R 777 public/layouts;
