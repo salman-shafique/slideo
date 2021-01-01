a61536fcc04c41181bc1932b87b57866efcd044b
Alperen Berat DURMUŞ <44159011+alperendurmus@users.noreply.github.com>
2021-01-01 17:51:43 +0300

    Enter to release textbox

--- assets/editor/js/shapes/textbox/createForeignObject.js
+++ assets/editor/js/shapes/textbox/createForeignObject.js
@@ -1,8 +1,10 @@
-import stringToDOM from "Editor/js/utils/stringToDOM";
+import reactToDOM from "Editor/js/utils/reactToDOM";
+import React from "react";
+import deSelectAll from "Editor/js/shapes/actions/drag/utils/deSelectAll";
 
 export default function createForeignObject(svg, positionObj = { x: 2000, y: 2000, width: 2000, height: 2000 }) {
     // Generate the foreign object
-    let foreignObject = svg.createElementNS("http://www.w3.org/2000/svg", "foreignObject");
+    const foreignObject = svg.createElementNS("http://www.w3.org/2000/svg", "foreignObject");
     foreignObject.setAttribute("x", positionObj.x);
     foreignObject.setAttribute("y", positionObj.y);
     foreignObject.setAttribute("width", positionObj.width);
@@ -10,16 +12,22 @@ export default function createForeignObject(svg, positionObj = { x: 2000, y: 200
     foreignObject.setAttribute("class", "bounding_box");
 
     // Clone the template
-    let contentTemplate = stringToDOM(`
-        <table style="width:100%">
-            <tbody style="padding: 0;">
-                <tr style="padding: 0;">
-                    <td style="padding: 0;line-height: 1.23;">
+    const contentTemplate = reactToDOM(
+        <table style={{ "width": "100%" }}>
+            <tbody style={{ "padding": "0" }}>
+                <tr style={{ "padding": "0" }}>
+                    <td style={{ "padding": "0", "lineHeight": "1.23" }}>
                     </td>
                 </tr>
             </tbody>
         </table>
-    `);
+    );
+    const td = contentTemplate.querySelector("td");
+    td.onkeyup = (event) => {
+        if (event.key == 'Enter') {
+            deSelectAll();
+        }
+    }
 
     foreignObject.appendChild(contentTemplate);
 
