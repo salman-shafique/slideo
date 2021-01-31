a6b910ac56be52a394f4c2663ddacb00b188b8d8
Alperen Berat DURMUŞ <44159011+alperendurmus@users.noreply.github.com>
2021-01-31 13:04:48 +0300

    getOpacity

--- assets/editor/js/sidebar/components/OpacitySlider/utils.js
+++ assets/editor/js/sidebar/components/OpacitySlider/utils.js
@@ -1,9 +1,21 @@
+import shape from "Editor/js/entity/shape";
+import session from "Editor/js/session";
+import constants from "Editor/js/constants";
+import getShapeType from "Editor/js/shapes/actions/drag/utils/getShapeType";
+
 /**
  * 
  * @param {SVGGElement} g 
  */
 export function getOpacity(g) {
+    const shapeId = g.getAttribute("shape_id");
+    const shape_ = shape(session.CURRENT_SLIDE, shapeId);
+    const data = shape_.data();
+    if (data.shape_opacity != undefined)
+        return parseFloat(data.shape_opacity);
 
+    if (g.getAttribute("shape_opacity"))
+        return parseFloat(g.getAttribute("shape_opacity"));
 
-    return Math.random();
+    return 1;
 }
