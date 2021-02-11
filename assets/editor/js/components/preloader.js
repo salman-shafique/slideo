6a40b29fd786b4c140a0a1cf876f16e40b59396b
Alperen Berat DURMUŞ <44159011+alperendurmus@users.noreply.github.com>
2021-02-11 12:53:47 +0300

    update preloader

--- assets/editor/js/components/preloader.js
+++ assets/editor/js/components/preloader.js
@@ -4,12 +4,17 @@ let registeredEvents = 0;
 function preloaderClass() {
     if (!(this instanceof preloaderClass)) return new preloaderClass(...arguments);
     const preloaderEl = document.querySelector(".preloader-container");
-    this.show = () => {
+    this.show = (callback = null) => {
         registeredEvents++;
         if (preloaderEl.classList.contains("d-none")) {
             preloaderEl.classList.add("d-flex");
             preloaderEl.classList.remove("d-none");
         }
+        if (callback)
+            setTimeout(() => {
+                callback();
+                this.hide();
+            }, 500);
     }
     this.hide = () => {
         registeredEvents--;
