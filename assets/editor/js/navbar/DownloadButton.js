db07a6196e4ed1db1cfabd76ea7ca194a595a0c1
Alperen Berat DURMUŞ <44159011+alperendurmus@users.noreply.github.com>
2021-02-11 16:02:31 +0300

    Download modal

--- assets/editor/js/navbar/DownloadButton.js
+++ assets/editor/js/navbar/DownloadButton.js
@@ -2,27 +2,28 @@ import React from "react";
 import apiService from "Editor/js/utils/apiService";
 import session from "Editor/js/session";
 import { saveChanges } from "./SaveButton";
-import toastr from "Editor/js/components/toastr";
+import preloader from "Editor/js/components/preloader";
 import reactToDOM from "Editor/js/utils/reactToDOM";
 
 export default function DownloadButton() {
+
+    React.useEffect(() => {
+        $('#downloadModal').modal({
+            show: false,
+            focus: false
+        });
+    }, []);
+
     const download = () => {
+        preloader.show();
         saveChanges(() => {
             apiService({
                 url: "/api/presentation/download/start/" + session.PRESENTATION.presentationId,
                 success: (r) => {
                     if (r.success) {
-                        const alertBox = reactToDOM(
-                            <a
-                                href={"/editor/" + session.PRESENTATION.presentationId + "/download"}
-                                target="_blank"
-                                className="rtl"
-                                >
-                                ההורדה שלך עומדת בתור. לחץ כאן לצפייה בדף ההורדות.
-                            </a>
-                        );
-                        toastr.info(alertBox);
+                        $('#downloadModal').modal("show");
                     }
+                    preloader.hide();
                 }
             })
         });
@@ -30,8 +31,30 @@ export default function DownloadButton() {
 
 
     return (
-        <button onClick={download} className="btn btn-danger bevel-btn horizontal-text-clip">
-            <i className="fas fa-download mr-2"></i> הורדה
-        </button>
+        <>
+            <button onClick={download} className="btn btn-danger bevel-btn horizontal-text-clip">
+                <i className="fas fa-download mr-2"></i> הורדה
+            </button>
+            <div id="downloadModal" className="modal fade" role="dialog"
+                style={{
+                    "backgroundColor": "rgba(255,255,255,0.8)"
+                }} >
+                <div className="modal-dialog modal-dialog-centered" role="document">
+                    <div className="modal-content text-center" style={{ border: "none" }}>
+                        <div className="modal-body">
+                            <img src="/img/party.gif" />
+                            <h3>Congratulations!</h3>
+                            <h4>Your presentation is queued!</h4>
+                            <a href={"/editor/" + location.pathname.split("/").pop() + "/download"} target="_blank">
+                                <button className="btn btn-info">
+                                    <i className="fas fa-download mr-2"></i>
+                                    Go to downloads page
+                                </button>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div >
+        </>
     );
 }
