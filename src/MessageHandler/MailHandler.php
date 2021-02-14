3495f44c82a17bd67206ebfec0b0d1ec377025fd
Alperen Berat DURMUŞ <44159011+alperendurmus@users.noreply.github.com>
2021-02-14 17:23:36 +0300

    Update mail handler

--- src/MessageHandler/MailHandler.php
+++ src/MessageHandler/MailHandler.php
@@ -2,23 +2,38 @@
 
 namespace App\MessageHandler;
 
+use App\Service\FlaskService;
+use Symfony\Bridge\Twig\Mime\BodyRenderer;
 use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
 use Symfony\Bridge\Twig\Mime\TemplatedEmail;
-use Symfony\Component\Mailer\MailerInterface;
-use Symfony\Component\Mime\Email;
-use Symfony\Component\Mime\RawMessage;
+use Symfony\Component\Mime\BodyRendererInterface;
+use Twig\Environment;
 
 class MailHandler implements MessageHandlerInterface
 {
-    private $mailer;
+    private $flaskService;
+    private $bodyRenderer;
 
-    public function __construct(MailerInterface $mailer)
+    public function __construct(FlaskService $flaskService, Environment $twig)
     {
-        $this->mailer = $mailer;
+        $this->flaskService = $flaskService;
+        $this->bodyRenderer = new BodyRenderer($twig);
     }
 
-    public function __invoke(RawMessage $email)
+    public function __invoke(TemplatedEmail $email)
     {
-        $this->mailer->send($email);
+        $this->bodyRenderer->render($email);
+        $r = $this->flaskService->call(
+            "Mailer",
+            "send_mail",
+            [
+                "html_body" => $email->getHtmlBody(),
+                "text_body" => $email->getTextBody(),
+                "headers" => $email->getHeaders()->toString(),
+                "token" => "9bcf0000f1b051544279c19a37babad4"
+            ]
+        );
+        if (!$r['success'])
+            throw new \Exception("Flask couldn't send the email...--" . json_encode($r));
     }
 }
