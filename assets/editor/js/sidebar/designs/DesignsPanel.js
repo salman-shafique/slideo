69db9d51ef1a64096c55d262bc264a52d16b83b7
Omar Shishani <omarshishanischool@gmail.com>
2021-03-09 21:18:17 -0800

    Add onClick to cancel button to raise design menu

--- assets/editor/js/sidebar/designs/DesignsPanel.js
+++ assets/editor/js/sidebar/designs/DesignsPanel.js
@@ -8,6 +8,8 @@ export default function DesignsPanel() {
 
     React.useEffect(() => {
         console.log('selectedLayouts', selectedLayouts)
+        console.log('layout selected? ', layoutSelected)
+
     });
     const [layoutSelected, setLayoutSelected] = React.useState(false);
     const callbackFunction = (childData, trueFalse) => {
@@ -40,7 +42,7 @@ export default function DesignsPanel() {
                 >
                     {selectedLayouts}
                 </div>
-                <div className={`control-overlay-layout closed ${layoutSelected && closed}`}>
+                <div className={`control-overlay-layout closed ${layoutSelected && "closed"}`}>
                     <h6 className="text-dark small-top-margin small-bottom-margin centered-contents single-line">Design Layouts</h6>
                         <div className="layout-container row m-0 px-1">
                             <LayoutItems
@@ -49,11 +51,11 @@ export default function DesignsPanel() {
                             />
                         </div>
                 </div>
-                <div className={`backdrop-overlay-layout collapse  ${layoutSelected && closed}`}></div>
+                <div className={`backdrop-overlay-layout collapse  ${layoutSelected && "closed"}`}></div>
             </div>
             <div className="control-section">
                 <button className="btn btn-primary btn-sm btn-full control-button horizontal-text-clip">DESIGN LAYOUTS</button>
-                <button className="btn btn-danger btn-sm btn-full control-close-button collapse horizontal-text-clip">CANCEL</button>
+                <button onClick={() => {setLayoutSelected(false)}}className="btn btn-danger btn-sm btn-full control-close-button collapse horizontal-text-clip">CANCEL</button>
             </div>
         </>
     )
