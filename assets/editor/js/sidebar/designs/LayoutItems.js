7669051ca8ea1148abbd0c3601f035fc86a4a9a2
Omar Shishani <omarshishanischool@gmail.com>
2021-03-11 11:35:39 -0800

    Remove layoutSelected & callbackFunction fr DesignsPanel

--- assets/editor/js/sidebar/designs/LayoutItems.js
+++ assets/editor/js/sidebar/designs/LayoutItems.js
@@ -4,12 +4,6 @@ import slide from "Editor/js/entity/slide";
 import LayoutItem from "./LayoutItem";
 import preloader from "Editor/js/components/preloader";
 
-
-//Need to check if one layout is selected.
-//NEED To set layouts in designspanel, or find a way to keep the layouts showing, even though the whole design panel is being hidden.
-//If I can pass state of selected layouts back up to design panel, i should be able to send it back down to layoutitems
-
-
 export default function LayoutItems(props) {
 
     const [layouts, setLayouts] = React.useState({});
