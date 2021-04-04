b1babe96cc1132b6db856642ab9c736f737eb986
Alperen Berat DURMUŞ <44159011+alperendurmus@users.noreply.github.com>
2021-04-04 21:08:49 +0300

    Update index.js

--- assets/editor/index.js
+++ assets/editor/index.js
@@ -4,9 +4,3 @@ import "Editor/css/index.css";
 
 // Js
 import "Editor/js/index.js";
-
-// window.onload = function(){
-//     document.getElementsByClassName("main-container")[0].addEventListener("keyup", function(e){
-//         console.log(e.key);
-//     })
-// }
