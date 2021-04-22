c7862a3e08fe2d76853b7df9d9b1e5ed13e53e98
Alperen Berat DURMUŞ <alperenberatdurmus@hotmail.com>
2021-04-22 12:05:22 +0300

    Update updateMiniPrev.js

--- assets/editor/js/slides/updateMiniPrev.js
+++ assets/editor/js/slides/updateMiniPrev.js
@@ -2,12 +2,14 @@ import Events from "Editor/js/Events";
 import slide from "../entity/slide";
 import session from "../session";
 
-
-
 const updateMiniPrev = (event) => {
-    
     slide(session.CURRENT_SLIDE).cloneToMiniPrev();
 }
 
+Events.listen("shape.drag.ended", updateMiniPrev);
+Events.listen("shape.resize.ended", updateMiniPrev);
+Events.listen("shape.textbox.edit.ended", updateMiniPrev);
+Events.listen("shape.icon.changed", updateMiniPrev);
+Events.listen("shape.image.changed", updateMiniPrev);
 Events.listen("slide.preview.update", updateMiniPrev);
 Events.listen("shape.drag.ended", updateMiniPrev);
