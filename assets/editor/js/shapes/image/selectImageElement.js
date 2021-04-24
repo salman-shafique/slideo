3d6d5c49e0f59ac026c7cef643ef3c933725b9fc
Alperen Berat DURMUŞ <alperenberatdurmus@hotmail.com>
2021-04-24 17:43:42 +0300

    Disable auto open sidebar

--- assets/editor/js/shapes/image/selectImageElement.js
+++ assets/editor/js/shapes/image/selectImageElement.js
@@ -13,6 +13,5 @@ export default function selectImageElement(event) {
     const shapeId = g.getAttribute("shape_id");
     const shape_ = shape(session.CURRENT_SLIDE, shapeId);
     const shapeData = shape_.data();
-    sidebar.open("Images_Tool");
     addToImagesBar(shapeData.keyword);
 }
