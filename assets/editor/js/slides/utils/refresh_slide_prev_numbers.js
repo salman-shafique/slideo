0fa7763a4f2e3e6ce09c405016f6a5a10e5c966a
Alperen Berat DURMUŞ <alperenberatdurmus@hotmail.com>
2021-05-04 20:58:23 +0300

    Update refresh_slide_prev_numbers.js

--- assets/editor/js/slides/utils/refresh_slide_prev_numbers.js
+++ assets/editor/js/slides/utils/refresh_slide_prev_numbers.js
@@ -1,7 +1,7 @@
 import slide from "Editor/js/entity/slide";
 
 export default function refresh_slide_prev_numbers() {
-    let slide_prevs = document.querySelectorAll(".slide-thumbnail[data-slide-id]");
+    let slide_prevs = document.querySelectorAll(".slide-thumbnail[data-slide-id][is-active='true']");
     if (slide_prevs)
         slide_prevs.forEach((slide_prev, index) => {
             slide_prev.querySelector("span.slide-sl").innerText = index + 1;
