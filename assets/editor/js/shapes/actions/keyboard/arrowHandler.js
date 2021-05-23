e00d45feab58c1830e01537937efa8cfe9fc5739
Alperen Berat DURMUŞ <44159011+alperendurmus@users.noreply.github.com>
2021-05-23 14:42:51 +0300

    Rm arrow handler

--- assets/editor/js/shapes/actions/keyboard/arrowHandler.js
+++
@@ -1,3 +0,0 @@
-export default function arrowHandler (event){
-    console.log(event);
-}
--- assets/editor/js/shapes/actions/keyboard/index.js
+++ assets/editor/js/shapes/actions/keyboard/index.js
@@ -2,10 +2,6 @@ import deSelectAll from "Editor/js/shapes/actions/drag/utils/deSelectAll";
 import deleteShapes from "Editor/js/shapes/actions/delete/deleteShapes";
 import session from "Editor/js/session";
 import { undo, redo } from "Editor/js/history/index";
-import arrowHandler from "./arrowHandler";
-import reactToDOM from "Editor/js/utils/reactToDOM";
-import React from "react";
-import constants from "Editor/js/constants";
 import { shapeHandler } from "Editor/js/components/ContextMenu.js"
 import shape from "Editor/js/entity/shape";
 import toastr from "Editor/js/components/toastr";
@@ -77,16 +73,7 @@ const keyboardHandler = (event) => {
  */
 export default function keyboardListener(contentDocument) {
 
-    const rect = reactToDOM(
-        <rect x="0" y="0" width={constants.SVG_WIDTH()} height={constants.SVG_HEIGHT()} fill="white"/>,
-        null,
-        "http://www.w3.org/2000/svg"
-    )
-    contentDocument.querySelector(".SlideGroup .Slide").prepend(rect);
-
-
     contentDocument.addEventListener('keyup', keyboardHandler);
-    contentDocument.addEventListener('keydown', arrowHandler);
 
 }
 
@@ -94,4 +81,3 @@ export default function keyboardListener(contentDocument) {
  * @description For the window, one time
  */
 window.addEventListener('keyup', keyboardHandler);
-// window.addEventListener('keydown', arrowHandler);
