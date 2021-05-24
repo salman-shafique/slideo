d48c33415438bd20d3e15d485615ac54f6011339
Alperen Berat DURMUŞ <44159011+alperendurmus@users.noreply.github.com>
2021-05-24 10:58:48 +0300

    Update arrowHandler.js

--- assets/editor/js/shapes/actions/keyboard/arrowHandler.js
+++ assets/editor/js/shapes/actions/keyboard/arrowHandler.js
@@ -1,46 +1,39 @@
 import session from "Editor/js/session";
-import deSelectAll from "../drag/utils/deSelectAll";
+import constants from "Editor/js/constants";
+import { relocateResizeCircleContainer } from "Editor/js/shapes/actions/resize/utils/copyTransform";
 
 let width_1 = 0;
 let width_2 = 0;
 
-export default function arrowHandler (event){
-session.SELECTED_ELEMENTS.forEach(selectedEl => {
-        if(event.which === 37){
-            width_1 += -500
-            width_2 += 0
-            selectedEl.translate.transform.setTranslate(
-                selectedEl.translate.startingE + width_1,
-                selectedEl.translate.startingF + width_2 ,
-           );
-        }
-       else if(event.which === 38){
-        width_2 += -500
-        width_1 += 0
-            selectedEl.translate.transform.setTranslate(
-                selectedEl.translate.startingE +    width_1,
-                selectedEl.translate.startingF +    width_2
-            );
-        }
-        else if(event.which === 39){
-            width_1 += 500
-            width_2 += 0
-            selectedEl.translate.transform.setTranslate(
-                selectedEl.translate.startingE +  width_1,
-                selectedEl.translate.startingF + width_2 ,
-            );
-        }
-        else if(event.which === 40){
-            width_2 += 500
-            width_1 += 0
-            selectedEl.translate.transform.setTranslate(
-                selectedEl.translate.startingE +  width_1 ,
-                selectedEl.translate.startingF +   width_2,
-            );
-
-        }
-        else{
-            deSelectAll()
-        }
-})
+export default function arrowHandler(event) {
+    if (session.TEXT_EDITING) return;
+    if (session.SHAPE_STATE == constants.SHAPE_STATES.PREVIEW) return;
+    
+    switch (event.which) {
+        case 37:
+            width_1 = -500;
+            width_2 = 0;
+            break;
+        case 38:
+            width_1 = 0;
+            width_2 = -500;
+            break;
+        case 39:
+            width_1 = 500;
+            width_2 = 0;
+            break;
+        case 40:
+            width_2 = 500;
+            width_1 = 0;
+            break;
+        default:
+            return;
+    }
+    session.SELECTED_ELEMENTS.forEach(selectedEl => {
+        selectedEl.translate.transform.setTranslate(
+            selectedEl.translate.transform.matrix.e + width_1,
+            selectedEl.translate.transform.matrix.f + width_2
+        );
+        relocateResizeCircleContainer(selectedEl.shape);
+    })
 }
