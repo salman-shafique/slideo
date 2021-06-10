d1445047c938fd89e51a08bc617547c5cdb8f86d
citrafriska <cfriskanita@gmail.com>
2021-06-10 16:54:00 +0700

    Added Image_tab_with_selected_Preset_Keyword_related_to_slide function

--- assets/editor/js/sidebar/images/index.js
+++ assets/editor/js/sidebar/images/index.js
@@ -6,16 +6,34 @@ import toggleKeyword from "./toggleKeyword";
 import "./searchBox";
 import "./upload";
 import Events from "Editor/js/Events";
+import {addToImagesBar} from "./searchBox"
+import React from "react"
 
 const showEmptyList = () => {
     $('#Images_Panel div[data-keyword].search-keyword').removeClass("active");
     $('#Images_Panel div[data-keyword].search-keyword').css("opacity", "0.5");
     $('#sideBarImages div[data-keyword].image-container').hide();
     document.querySelector("#sideBarImagesEmpty").style.display = "";
-}
+} 
+
+Events.listen("presentation.inited", () => {
+    const slide = session.PRESENTATION.slides.map(e => e.analyzedContent)
+    const analizedc = slide.map(e => e.map(d => d))
+    const d = analizedc.map(k=> k[0])
+   
+    d.map(k => {
+        for (const p in k) {
+        if(k[p]){
+            k[p].data.keyword ? addToImagesBar(k[p].data.keyword) : null;
+        }
+      }
+    })
+})
+
 
 // Show empty image list when all released
 Events.listen("shape.allReleased", showEmptyList);
+
 Events.listen("shape.selected", () => {
     if (session.SELECTED_ELEMENTS.length != 1) return;
     const g = session.SELECTED_ELEMENTS[0].shape;
@@ -26,8 +44,12 @@ Events.listen("shape.selected", () => {
         if (data.keyword)
             toggleKeyword(data.keyword);
 
+
         return;
     };
 
     showEmptyList();
 })
+
+
+
