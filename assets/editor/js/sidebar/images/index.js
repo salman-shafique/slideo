b6b09c230a2cace627d8f44ae7dd8baa5f8b7d8f
Alperen Berat DURMUŞ <44159011+alperendurmus@users.noreply.github.com>
2021-06-10 21:42:59 +0300

    Update index.js

--- assets/editor/js/sidebar/images/index.js
+++ assets/editor/js/sidebar/images/index.js
@@ -6,16 +6,10 @@ import toggleKeyword from "./toggleKeyword";
 import "./searchBox";
 import "./upload";
 import Events from "Editor/js/Events";
-import {addToImagesBar} from "./searchBox"
+import { addToImagesBar } from "./searchBox"
 
-const showEmptyList = () => {
-    $('#Images_Panel div[data-keyword].search-keyword').removeClass("active");
-    $('#Images_Panel div[data-keyword].search-keyword').css("opacity", "0.5");
-    $('#sideBarImages div[data-keyword].image-container').hide();
-    document.querySelector("#sideBarImagesEmpty").style.display = "";
-} 
-
-Events.listen("shape.allReleased", () => {
+// Insert keywords to images sidebar
+Events.listen("presentation.inited", () => {
     const slides = session.PRESENTATION.slides.map(slide => slide.analyzedContent)
     const analyzedContents = slides.map(a => a.map(ac => ac))
     const analyzedData = analyzedContents.map(e => e)
@@ -24,14 +18,12 @@ Events.listen("shape.allReleased", () => {
         const datas = d[0]
         const dataObjArr = Object.keys(datas)
         const keywords = dataObjArr.map(d => datas[d])
-        keywords.map((key,i)  => {
-          typeof keywords[i] === "object" && keywords[i] !== null && keywords[i].data && keywords[i].data.keyword ?
-          addToImagesBar(keywords[i].data.keyword) : null
+        keywords.map((key, i) => {
+            typeof keywords[i] === "object" && keywords[i] !== null && keywords[i].data && keywords[i].data.keyword ?
+                addToImagesBar(keywords[i].data.keyword) : null
         })
     })
-   
-    // Show empty image list when all released
-    showEmptyList
+
 })
 
 Events.listen("shape.selected", () => {
@@ -40,15 +32,13 @@ Events.listen("shape.selected", () => {
     if (getShapeType(g) == constants.SHAPE_TYPES.IMAGE) {
         const shape_ = shape(session.CURRENT_SLIDE, g.getAttribute("shape_id"));
         const data = shape_.data();
-        if(!data) return;
+        if (!data) return;
         if (data.keyword)
             toggleKeyword(data.keyword);
 
 
         return;
     };
-
-    showEmptyList();
 })
 
 
