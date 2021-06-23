9edabc9a7e14c0431ede700c8fa4cb6bb0827332
citrafriska <cfriskanita@gmail.com>
2021-06-23 12:20:37 +0700

    Removed New Presentation Name Edit from Editor Page and Made the EDIT NAME option for each presentation in the MY ACCOUNT section

---
+++ assets/account/presentation/change_name.js
@@ -0,0 +1,27 @@
+import add_event from "Editor/js/utils/add_event";
+import apiService from "Editor/js/utils/apiService";
+
+const input = document.getElementsByClassName('ppt-title');
+Array.from(input).forEach(e => {
+    e.style.width = (e.value.length - 1)+ "ch";
+})
+
+add_event(".ppt-title", "change", function (e) {
+
+    if(e.target.style.width < "190px"){
+        e.target.style.width = (e.target.value.length - 1) + "ch";
+    }
+
+    let presentation_name = e.target.value;
+    let presentation_id = e.target.id;
+    if (!presentation_name) return;
+
+    apiService({
+        url: `/api/presentation/change_name/${presentation_id}`,
+        data: {
+            "presentation_name": presentation_name
+        },
+        success: (response) => {
+        },
+    });
+})
