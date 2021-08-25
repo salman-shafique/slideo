d465fb6e6aa8fceff3ed67e60dcb0e997059e932
Citra Friskanita <cfriskanita@gmail.com>
2021-08-26 02:23:50 +0700

    Updated Save Changes Function

--- assets/editor/js/slides/saveChanges.js
+++ assets/editor/js/slides/saveChanges.js
@@ -1,12 +1,17 @@
 import Events from "Editor/js/Events";
 import session from "../session";
+import preloader from "Editor/js/components/preloader";
 import { saveChanges } from "Editor/js/navbar/SaveButton";
 
-const save = async (event) => {
+const save = (event) => {
     if (!session.INITED) return;
     if (!session.PRESENTATION) return;
-    await saveChanges()
+    if (!session.SAVED && event.type === "saveChange.updated") return
+    console.log(event.type);
+    saveChanges()
 }
 
-Events.listen("shape.released", save);
-Events.listen("slide.preview.updateAll", save);
+Events.listen("saveChange.inited", save);
+Events.listen("saveChange.updated", save);
+Events.listen("slide.deleted", save);
+
