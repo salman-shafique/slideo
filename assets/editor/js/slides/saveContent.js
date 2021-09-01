66eb7dbc71e118d129c14d693e173eb0ce3809ea
Citra Friskanita <cfriskanita@gmail.com>
2021-09-02 05:12:05 +0700

    Added save content function

--- assets/editor/js/slides/saveContent.js
+++ assets/editor/js/slides/saveContent.js
@@ -1,54 +1,11 @@
 import Events from "Editor/js/Events";
 import apiService from "Editor/js/utils/apiService";
-import slide from "Editor/js/entity/slide";
-import shape from "Editor/js/entity/shape";
-
-
-
-// Json Body:
-
-// {
-//     "id": 2392,
-//     "keyword": "slideTitle",
-//     "data": {
-//         "lang": "he",
-//         "text": "זה זה ",
-//         "title": "זה זה",
-//         "keyword": "It",
-//         "keywords": [
-//             "It"
-//         ],
-//         "direction": "rtl",
-//         "inEnglish": "It"
-//     }
-// }
-
-
-// {
-//     "id":3997,
-//     "keyword":"slideTitle",
-//         "data": {
-//         "lang": "en",
-//         "text": "Test API",
-//         "title": "Do Eiusmod Tempor Incididunt",
-//         "keyword": "tempor",
-//         "keywords": [
-//             "tempor",
-//             "incididunt"
-//         ],
-//         "direction": "ltr",
-//         "inEnglish": "do eiusmod tempor incididunt"
-//     }
-// }
-
 
 const saveContent = (event) => {
-
     const slides = session.PRESENTATION.slides;
     const slide = slides.find(slide => slide.slideId == event.data.slideId)
     const shape_ = slide.shapes.find(aShape => aShape.data.shape_id == event.data.shapeId);
-    const shapeData = shape_.data
-    
+    const shapeData = shape_.data    
 
     let request = () => {
         if(shapeData.alt == "slidetitle"){
@@ -61,21 +18,17 @@ const saveContent = (event) => {
             return data   
         }else {
 
-            // originalSentence, h1
+            // icon, h1Image, originalSentence, h1
             const type = shapeData.icon ? "icon" : shapeData.image ? "h1Image" : shapeData.alt == "paragraph|0" ? "originalSentence" : "h1"
             const payload = slide.analyzedContent[0][type]
-            // payload.data.keyword = payload.data.keywords[0]
-
-            // payload.x = event.data.newX
-            // payload.y = event.data.newY
-
-            console.log(payload);
-
+            payload.data.keyword = payload.data.keywords[0]
+           
             const data =  {
                 "id": payload.id,
                 "keyword": payload.keyword,
                 "data": payload.data
             }
+
             return data  
         }
     }
@@ -90,7 +43,8 @@ const saveContent = (event) => {
 
     // sendBeacon
     navigator.sendBeacon("/api/presentation/save/content", data);
+
 }
 
 
-// Events.listen("saveChange.updated", saveContent)
+Events.listen("saveChange.content", saveContent)
