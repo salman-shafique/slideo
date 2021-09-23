48736b44e2606d20e04ec6f547227d2220bb1c09
Citra Friskanita <cfriskanita@gmail.com>
2021-09-23 14:08:14 +0700

    Fixed loading stuck

--- assets/editor/js/slides/updateMiniPrev.js
+++ assets/editor/js/slides/updateMiniPrev.js
@@ -4,25 +4,21 @@ import session from "../session";
 import preloader from "Editor/js/components/preloader";
 
 const updateMiniPrev = (event) => {
-    slide(session.CURRENT_SLIDE).cloneToMiniPrev();
-}
+  slide(session.CURRENT_SLIDE).cloneToMiniPrev();
+};
 const updateAllMiniPrev = (event) => {
-    if(event.type == "saveChange.inited"){
-       preloader.hide()
-    }else if(!session.SAVED){
-       preloader.show()
-    }
+  if (event.type == "saveChange.inited") {
+    preloader.hide();
+    Events.saveChange.thumbnail();
+  } else if (!session.SAVED) {
+    preloader.show();
+  }
 
-    session.PRESENTATION.slides.forEach(slide_ => {
-        slide(slide_.slideId).cloneToMiniPrev();
-    })
-}
+  session.PRESENTATION.slides.forEach((slide_) => {
+    slide(slide_.slideId).cloneToMiniPrev();
+  });
+};
 
-// Events.listen("shape.drag.ended", updateMiniPrev);
-// Events.listen("shape.resize.ended", updateMiniPrev);
-// Events.listen("shape.textbox.edit.ended", updateMiniPrev);
-// Events.listen("shape.icon.changed", updateMiniPrev);
-// Events.listen("shape.image.changed", updateMiniPrev);
 Events.listen("shape.released", updateMiniPrev);
 Events.listen("slide.preview.updateAll", updateAllMiniPrev);
 Events.listen("saveChange.inited", updateAllMiniPrev);
