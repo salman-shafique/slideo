5ddb9f1c15ddaa38c79f17a687059de6ba13546f
Citra Friskanita <cfriskanita@gmail.com>
2021-09-27 03:40:44 +0700

    Updated save reset default design's shapes

--- assets/editor/js/slides/saveStyle.js
+++ assets/editor/js/slides/saveStyle.js
@@ -3,6 +3,8 @@ import apiService from "Editor/js/utils/apiService";
 import session from "../session";
 import slide from "Editor/js/entity/slide";
 
+let isStyleChanged = false;
+
 const saveStyle = (event) => {
   const currentSlide = slide(session.CURRENT_SLIDE);
 
@@ -17,25 +19,73 @@ const saveStyle = (event) => {
   const data = JSON.stringify(request);
 
   const currentShapes = currentSlide.slideData().shapes;
-  const lastShapeId = currentShapes[currentShapes.length - 1].id;
-  request.shapes.forEach((shape, i) => {
-    shape.id = lastShapeId + i + 1;
-  });
+  const slides = session.PRESENTATION.slides;
+  let lastShapeId = 0;
+  slides.forEach((slide) =>
+    slide.shapes.forEach((shape) => {
+      const id = parseInt(shape.id);
+      if (id > lastShapeId) {
+        lastShapeId = id;
+      }
+    })
+  );
 
+  if (isStyleChanged == true) {
+    request.shapes.forEach((shape, i) => {
+      shape.id = lastShapeId + i + 1;
+      shape.data.isStyleChanged = true;
+
+      if (i == request.shapes.length - 1) {
+        lastShapeId = shape.id;
+      }
+    });
+  } else {
+    request.shapes.forEach((shape, i) => {
+      shape.id = lastShapeId + i + 2;
+      shape.data.isStyleChanged = true;
+
+      if (i == request.shapes.length - 1) {
+        lastShapeId = shape.id - 1;
+      }
+    });
+  }
   event.data.designData.shapes = request.shapes;
   currentSlide.changeDesign(event.data.designData);
+  Events.saveChange.background();
 
-  // AJAX
+  // AJAX;
   // apiService({
   //   url: "/api/presentation/save/style",
   //   data: data,
   //   success: (r) => {
-  //     console.log("NEW SHAPES", r.newShapes);
+  //     console.log("RES", r.newShapes);
   //   },
   // });
 
   // sendBeacon
   navigator.sendBeacon("/api/presentation/save/style", data);
+
+  isStyleChanged = true;
+};
+
+const checkIsChanged = (event) => {
+  const currentSlide = slide(session.CURRENT_SLIDE);
+  const shapes = currentSlide.slideData().shapes;
+
+  shapes.forEach((shape) => {
+    if (shape.data.alt == "h1image|0" || shape.data.icon) {
+      if (shape.data.isStyleChanged) {
+        isStyleChanged = true;
+      } else {
+        isStyleChanged = false;
+      }
+    }
+  });
 };
 
 Events.listen("saveChange.style", saveStyle);
+Events.listen("saveChange.inited", checkIsChanged);
+Events.listen("slide.display", (event) => {
+  if (!session.SAVED) return;
+  checkIsChanged(event);
+});
